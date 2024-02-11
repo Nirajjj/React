27f000a78b3daabd7a40175b48ecb406fc06726e
Niraj Parab <nirajparab007@gmail.com>
2024-02-11 10:04:37 +0530

    update

---
+++ src/components/userClass.js
@@ -0,0 +1,57 @@
+import React from "react";
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      count2: 0,
+    };
+  }
+  render() {
+    const { name } = this.props;
+    console.log(name);
+    return (
+      <div className="about-card-container">
+        <div className="about-card">
+          <div>
+            <h1>Niraj Parab</h1>
+            <p>
+              🚀 Dynamic self-taught full-stack developer evolving from a
+              successful background in management. Proficient in both front-end
+              and back-end technologies, I bring a unique fusion of technical
+              prowess and robust soft skills. Adept at creative problem-solving
+              and collaborative efforts, I leverage strong communication and
+              interpersonal skills to drive innovation in diverse team
+              environments. Excited to seamlessly integrate technical and soft
+              skills in a vibrant tech setting!
+            </p>
+          </div>
+          <div>
+            <h1>Website Details</h1>
+            <p>
+              🚀Dine with dynamism! Book your next restaurant experience through
+              a captivating single-page React application, fueled by the live
+              power of the Swiggy Live API. This data-driven approach drives a
+              conflict-driven, single-page application (SPA) experience, where
+              real-time information seamlessly transforms the interface,
+              captivating users with an ever-evolving journey. Dive into live
+              availability updates, explore dynamic menus, and book your perfect
+              table instantly - all within a single, captivating website.
+            </p>
+            <h3>
+              <a
+                className="github-link"
+                href="https://github.com/Nirajjj/React"
+              >
+                click here to see the code
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
---
+++ src/utils/useRestaurantInfo.js
@@ -0,0 +1,32 @@
+import resInfo_API from "../utils/resInfo";
+import { useState, useEffect } from "react";
+function useRestaurantInfo(resID) {
+  const [resInfo, setresInfo] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const resInfo = await fetch(resInfo_API + resID, {
+      headers: {
+        "x-cors-api-key": "temp_d3d854db3c595f104dc6125d136a1eb1",
+      },
+    });
+    const jsonData = await resInfo.json();
+    console.log(
+      jsonData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
+        ?.card.card.itemCards ||
+        jsonData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
+          ?.card?.card.categories[0].itemCards
+    );
+    setresInfo(
+      jsonData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
+        ?.card.card.itemCards ||
+        jsonData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
+          ?.card?.card.categories[0].itemCards
+    );
+  };
+  return resInfo;
+}
+
+export default useRestaurantInfo;
